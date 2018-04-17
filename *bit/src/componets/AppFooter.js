0576ba9c58741cebe4b1e1950c2e22a16d068b47
Fredy Alvarado, Jr <fredwhyyy@gmail.com>
2018-04-17 10:49:31 -0500

    refactor: convert to stateless component as it does not require a component lifecycle or state

--- *bit/src/componets/AppFooter.js
+++ *bit/src/componets/AppFooter.js
@@ -4,44 +4,42 @@ import { NavLink } from "react-router-dom";
 import globalStyles from "../utils/globalStyles";
 import MyReadableLogo from "../media/logo.svg";
 
-class AppFooter extends Component {
-  AppFooterStyles = css`
-    margin: 3em 0 1em;
-    border-top: 0.0625rem dotted ${globalStyles.color.purple};
+const AppFooterStyles = css`
+  margin: 3em 0 1em;
+  border-top: 0.0625rem dotted ${globalStyles.color.purple};
 
-    &_logo-container {
-      margin-top: 2em;
-      text-align: center;
-    }
-
-    &_logo {
-      width: 3em;
-      height: auto;
-    }
-  `;
-
-  render() {
-    return (
-      <div className={this.AppFooterStyles} data-class="AppFooterStyles">
-        <div className={`${this.AppFooterStyles}_logo-container`}>
-          <NavLink
-            to="/"
-            exact
-            onClick={() => {
-              this.props.setCurrentView("default");
-            }}
-          >
-            <img
-              src={MyReadableLogo}
-              className={`${this.AppFooterStyles}_logo`}
-              alt="MyReadable App"
-            />
-            <span className="sr-only">MyReadble App</span>
-          </NavLink>
-        </div>
-      </div>
-    );
+  &_logo-container {
+    margin-top: 2em;
+    text-align: center;
   }
-}
+
+  &_logo {
+    width: 3em;
+    height: auto;
+  }
+`;
+
+const AppFooter = () => {
+  return (
+    <div className={AppFooterStyles} data-class="AppFooterStyles">
+      <div className={`${AppFooterStyles}_logo-container`}>
+        <NavLink
+          to="/"
+          exact
+          onClick={() => {
+            this.props.setCurrentView("default");
+          }}
+        >
+          <img
+            src={MyReadableLogo}
+            className={`${AppFooterStyles}_logo`}
+            alt="MyReadable App"
+          />
+          <span className="sr-only">MyReadble App</span>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
 
 export default AppFooter;
